2914040ef05d8716b247db1a6ab1e4964e488213
Justin-Wang-ICD2O1-01 <Justin.wang24@student.tdsb.on.ca>
2024-11-09 15:31:16 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -26,8 +26,8 @@ def q3():
   
 def q4():
   #Write Assignment code here
-  num4= input("Input food: ")
-  num5= input("Input drink: ")
+  num4= int(input("Input food: "))
+  num5= int(input("Input drink: "))
   print(bool(f"Your food and drink is {num4} != “pizza” and {num5} != "pop"))
 
 def q5():
